3e0f44c53c86498f95336e911260f02e2edbebef
smlsunxie <smlsun@gmail.com>
2018-07-05 16:18:37 +0800

    新增 test 定義 task 更新 completed 之 API spec

--- test/integration/user-creation-api.test.js
+++ test/integration/user-creation-api.test.js
@@ -57,4 +57,33 @@ describe.only('user creation page', function () {
       .and.to.have.property("title");
   });
 
+  it('透過 api 更新 task 之 completed 狀態', async function () {
+    let username = 'frank';
+    let user = await this.models.User.create({
+      username
+    });
+
+    let task = await this.models.Task.create({
+      title: 'frank task',
+      UserId: user.id,
+      completed: false
+    });
+
+    let taskData = {
+      completed: true
+    }
+
+    let response = await request(app)
+      .put(`/api/task/${task.id}`)
+      .send(taskData);
+    let result = response.body;
+
+    expect(result.task)
+      .to.be.an('object')
+      .and.to.have.property("title")
+      .and.to.have.property("completed");
+    expect(result.task.completed).to.equal(true);
+  });
+
+
 });
